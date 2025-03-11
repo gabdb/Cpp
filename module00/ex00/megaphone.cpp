6b7067d4f880c247e0cc019a8f63e4d8f59e6325
Gabriel Nyssens <gnyssens@mi-c2-p8.s19.be>
2025-03-11 16:42:53 +0100

    ex00

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,20 +6,21 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:44:28 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/03/11 14:48:07 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:34:16 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int main(int ac, char **av)
 {
-	if (!av[1])
+	if (1 == ac)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
 	{
 		for (int j = 0; av[i][j]; j++)
-			std::cout << (char)std::toupper(av[i][j]);
+			std::cout << static_cast<char> (std::toupper(av[i][j])); //plus safe que (char) casting
 	}
 	std::cout << std::endl;
     return 0;
