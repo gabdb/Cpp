7a99b9095216d0944466b0bab546c0babe8fd06c
Gabriel Nyssens <gnyssens@shi-r12-p4.s19.be>
2025-03-11 14:50:38 +0100

    a

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gnyssens <gnyssens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:44:28 by gnyssens          #+#    #+#             */
-/*   Updated: 2025/03/11 14:41:02 by gnyssens         ###   ########.fr       */
+/*   Updated: 2025/03/11 14:48:07 by gnyssens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
 	{
-		//std::cout << (char)
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
 	}
+	std::cout << std::endl;
     return 0;
 }
