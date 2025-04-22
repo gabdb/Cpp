ca9120e599359670754db9026ab383ff456f2906
Gabriel Nyssens <gnyssens@fu-r1-p3.s19.be>
2025-04-22 17:19:34 +0200

    suite

---
+++ module00/ex02/Account.cpp
@@ -0,0 +1,12 @@
+#include "Account.hpp"
+#include <ctime>
+#include <iostream>
+#include <string>
+
+//
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
