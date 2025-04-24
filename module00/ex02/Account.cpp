15de1f5ead9d81ae2296d649f2348b74e8328f2a
gabdb <gabrielnyssens@gmail.com>
2025-04-25 01:24:59 +0200

    rien

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -3,10 +3,34 @@
 #include <iostream>
 #include <string>
 
-//
+// 4 static variables of Class Account
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
+
+// Getters
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+// Constructors
 
