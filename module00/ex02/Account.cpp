5fc7a57ac4a04bf91f9dac51d3ef1cf9bfc036a1
gabdb <gabrielnyssens@gmail.com>
2025-04-28 01:48:02 +0200

    fini

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -74,3 +74,68 @@ void Account::makeDeposit(int deposit)
               << ";amount:" << this->_amount 
               << ";nb_deposits:" << this->_nbDeposits << std::endl; 
 }
+
+// static private
+
+void Account::_displayTimestamp(void)
+{
+    std::time_t now = std::time(nullptr);
+    std::tm *local_time = std::localtime(&now);
+
+    std::cout << '[' <<  (local_time->tm_year + 1900);
+    if (local_time->tm_mon + 1 < 10)  
+        std::cout << '0' << (local_time->tm_mon + 1);
+    else
+        std::cout << (local_time->tm_mon + 1);
+    if (local_time->tm_mday < 10)
+        std::cout << '0' << local_time->tm_mday << '_';
+    else
+        std::cout << local_time->tm_mday << '_';
+    if (local_time->tm_hour < 10)
+        std::cout << '0' << local_time->tm_hour;
+    else
+        std::cout << local_time->tm_hour;
+    if (local_time->tm_min < 10)
+        std::cout << '0' << local_time->tm_min;
+    else
+        std::cout << local_time->tm_min;
+    if (local_time->tm_sec < 10)
+        std::cout << '0' << local_time->tm_sec << "] ";
+    else
+        std::cout << local_time->tm_sec << "] ";
+}
+
+// normal functions
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    _displayTimestamp();
+    if (this->_amount < withdrawal)
+    {
+        std::cout << "index:" << this->_accountIndex <<";p_amount:" << this->_amount
+                  << ";withdrawals:refused" << std::endl; 
+        return false;
+    }
+    this->_amount -= withdrawal;
+    this->_nbWithdrawals += 1;
+    _totalAmount -= withdrawal;
+    _totalNbWithdrawals += 1;
+    std::cout << "index:" << this->_accountIndex << ";p_amount:"
+              << this->_amount + withdrawal << ";withdrawals:" << withdrawal
+              << ";amount:" << this->_amount 
+              << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl; 
+    return true;
+}
+
+int Account::checkAmount(void) const {
+    return this->_amount;
+}
+
+void Account::displayStatus(void) const
+{
+    _displayTimestamp();
+
+    std::cout << "index:" << this->_accountIndex << ";amount:"
+              << this->_amount << ";deposits:" << this->_nbDeposits
+              << ";withdrawals:" << this->_nbWithdrawals << std::endl; 
+}
