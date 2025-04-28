e63e884cc23dddf7912e5b76655743f76e659781
Gabriel Nyssens <gnyssens@fu-r1-p3.s19.be>
2025-04-28 14:35:55 +0200

    debut01

---
+++ module01/ex00/Zombie.hpp
@@ -0,0 +1,17 @@
+#pragma once //compiler directive, ensures file is not included multiple times
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+        void    announce( void );
+};
+
+Zombie* newZombie( std::string name );
+void    randomChump( std::string name );
