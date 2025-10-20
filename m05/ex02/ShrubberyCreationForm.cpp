4d1e064eedb8ed01656cd33b021d0855465cacaf
Gabriel Nyssens <gnyssens@mi-r3-p5.s19.be>
2025-10-20 15:00:52 +0200

    ewrgerdnfgh

--- m05/ex02/ShrubberyCreationForm.cpp
+++ m05/ex02/ShrubberyCreationForm.cpp
@@ -1,1 +1,38 @@
 
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm("ShrubberyForm", 145, 137), _target(target)
+{}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other) : AForm(other), _target(other._target)
+{}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {}
+
+ShrubberyCreationForm&  ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
+{
+    if (this != &other)
+        AForm::operator=(other);
+    return (*this);
+}
+
+void    drawtree(std::ofstream& file)
+{
+    file << TREE << std::endl;
+}
+
+void    ShrubberyCreationForm::execute(const Bureaucrat& bcrat) const
+{
+    if (!this->getIsSigned())
+        throw FormNotSignedException();
+    if (bcrat.getGrade() > this->getGradeToExec())
+        throw GradeTooLowException();
+    std::ofstream file((this->_target) + "_shrubbery");
+    if (!(file.is_open()))
+        throw std::runtime_error("Failed to open output file.");
+    else
+    {
+        drawtree(file);
+        file.close();
+    }
+}
--- m05/ex02/ShrubberyCreationForm.hpp
+++ m05/ex02/ShrubberyCreationForm.hpp
@@ -4,7 +4,26 @@
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 
+# define TREE "	 /\\n\
+    			/  \\n\
+     		   /++++\\n\
+    		  /  ()  \\n\
+      		    ||||\n\
+      			||||"
+
 class ShrubberyCreationForm : public AForm
 {
-    
+    private:
+		const std::string _target;
+		ShrubberyCreationForm();
+	
+	public:
+		ShrubberyCreationForm(std::string target);
+		ShrubberyCreationForm(const ShrubberyCreationForm& other);
+		~ShrubberyCreationForm();
+		ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
+
+		void	execute(const Bureaucrat& bcrat) const; //fonction virtual = 0; parent
 };
+
+void	drawtree(std::ofstream& file);
