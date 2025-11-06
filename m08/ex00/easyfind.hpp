fb7e38599b1a3b41008618d19486f55ca537c421
Gabriel Nyssens <gnyssens@fu-r1-p7.s19.be>
2025-11-06 16:27:11 +0100

    rien

--- m08/ex00/easyfind.hpp
+++ m08/ex00/easyfind.hpp
@@ -1,3 +1,16 @@
 
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <list>
+
+
+template <typename T> //T sera un type de container
+typename T::iterator	easyfind(T& container, int nb)
+{
+	
+}
