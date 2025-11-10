26ad1782a81c3249ce9ee68b19eab1023c9a7e10
Gabriel Nyssens <gnyssens@fu-r2-p7.s19.be>
2025-11-10 17:01:48 +0100

    m08_00 fini a priori

--- m08/ex00/easyfind.hpp
+++ m08/ex00/easyfind.hpp
@@ -12,5 +12,23 @@
 template <typename T> //T sera un type de container
 typename T::iterator	easyfind(T& container, int nb)
 {
-	
+	std::cout << "(Using regular easyfind): ";
+	typename T::iterator result = std::find(container.begin(), container.end(), nb);
+
+    if (result == container.end())
+        throw std::runtime_error("Value not found !");
+    return result;
 }
+
+//const overload pr gerer les inputs const
+template <typename T>
+typename T::const_iterator	easyfind(const T& container, int nb)
+{
+	std::cout << "(Using 'const' version of easyfind): ";
+	typename T::const_iterator result = std::find(container.begin(), container.end(), nb);
+
+	if (result == container.end())
+		throw std::runtime_error("Value not found !");
+	return result;
+}
+
--- m08/ex00/main.cpp
+++ m08/ex00/main.cpp
@@ -1,1 +1,37 @@
 
+#include "easyfind.hpp"
+
+int main(void)
+{
+    std::vector<int> vect;
+    for (int i = 0; i < 10; i++) {
+        vect.push_back(i * i);
+        std::cout << "vect[" << i << "]" << ": " << vect[i] << std::endl;
+    }
+    std::cout << std::endl;
+    try {
+        std::vector<int>::iterator it = easyfind(vect, 9);
+        std::cout << *it << std::endl;
+    } catch (std::exception& e) {
+        std::cout << "Error : " << e.what() << std::endl;
+    }
+
+    std::cout << "\n" << std::endl;
+    std::deque<int> deq;
+    for (int i = 0; i < 10; i++)
+    {
+        deq.push_back(i * 5);
+        std::cout << "deque[" << i << "]" << ": " << deq[i] << std::endl;
+    }
+
+    const std::deque<int> &ref_deq = deq; //version read-only de 'list'
+    std::cout << std::endl;
+    try {
+        std::deque<int>::const_iterator it = easyfind(ref_deq, 5);
+        std::cout << *it << std::endl;
+    } catch (std::exception& e) {
+        std::cout << "Error : " << e.what() << std::endl;
+    }
+
+    return 0;
+}
